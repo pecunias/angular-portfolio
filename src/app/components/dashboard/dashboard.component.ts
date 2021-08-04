0ef2ef7750bc00cd92596d5a483ef198468bec0b
pecunias <jelte_wiersma@hotmail.com>
2021-08-04 22:13:23 +0200

    Mobile responsive - Add responsive layout

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -9,13 +9,21 @@ import { PortfolioService } from 'src/app/services/portfolio/portfolio.service';
 })
 export class DashboardComponent implements OnInit {
   public positions: IPosition[] = [];
+  public breakpoint: number = 0;
+  public MAX_WIDTH_SCREEN: number = 600;
+
 
   constructor(
     private portfolioService: PortfolioService
   ) { }
 
   ngOnInit(): void {
+    this.breakpoint = (window.innerWidth <= this.MAX_WIDTH_SCREEN) ? 1 : 2;
     this.generatePositions();
+  }
+
+  onResize(event: any) {
+    this.breakpoint = (event.target.innerWidth <= this.MAX_WIDTH_SCREEN) ? 1 : 2;
   }
 
   generatePositions() {
